df0a99fc282fd4c4784e92311c11a2da3d0a960c
sungsphinx <sungsphinx@noreply.codeberg.org>
2023-09-19 19:27:14 +0000

    Update src/main.c

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ main (int   argc,
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
-	app = dog_application_new ("page.codeberg.sungsphinx.Dog", G_APPLICATION_DEFAULT_FLAGS);
+	app = dog_application_new ("page.codeberg.sungsphinx.DogGTK", G_APPLICATION_DEFAULT_FLAGS);
 	ret = g_application_run (G_APPLICATION (app), argc, argv);
 
 	return ret;
