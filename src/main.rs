a652ff29f29f57aa656331b628dc5a774c935fcb
sungsphinx <dreed4470@gmail.com>
2023-11-14 20:57:46 +0000

    Change copyright names

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /* main.rs
  *
- * Copyright 2023 User
+ * Copyright 2023 SOrg, 2023 sungsphinx
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
